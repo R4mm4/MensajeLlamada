a1ff18340c75838ec38a3495f39e6b8b769b1032
R4mm4 <65689530+R4mm4@users.noreply.github.com>
2021-02-24 00:16:26 -0600

    tiene que jalar

--- app/src/main/java/com/example/mensajellamada/MainActivity.java
+++ app/src/main/java/com/example/mensajellamada/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.mensajellamada;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
+import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,7 +21,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public String txtNumero,NumLlamada,txtMensaje;
 
+    public EditText number, message;
+    public Button send;
+
+    final int SEND_SMS_PERMISSION_REQUEST_CODE = 111;
+    private  Button mSendMessageBtn;
     private TelephonyManager mTelephonyManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button mDialButton = (Button) findViewById(R.id.btn_dial);
         final EditText mPhoneNoEt = (EditText) findViewById(R.id.et_phone_no);
+        number = findViewById(R.id.et_phone_no);
+        message = findViewById(R.id.et_message);
+        send=findViewById(R.id.btn_send_message);
 
         mDialButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,10 +45,50 @@ public class MainActivity extends AppCompatActivity {
                 String phoneNo = mPhoneNoEt.getText().toString();
                 if(!TextUtils.isEmpty(phoneNo)) {
                     String dial = "tel:" + phoneNo;
-                    startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
                 }else {
                     Toast.makeText(MainActivity.this, "Enter a phone number", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        Button sendMessageBtn = (Button) findViewById(R.id.btn_send_message);
+        final EditText messagetEt = (EditText) findViewById(R.id.et_message);
+        sendMessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = messagetEt.getText().toString();
+                String phoneNo = mPhoneNoEt.getText().toString();
+                if(!TextUtils.isEmpty(message) && !TextUtils.isEmpty(phoneNo)) {
+                    Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + phoneNo));
+                    smsIntent.putExtra("sms_body", message);
+                    startActivity(smsIntent);
+                }
+            }
+        });
+        mSendMessageBtn = (Button) findViewById(R.id.btn_send_message);
+
+        mSendMessageBtn.setEnabled(false);
+        if(checkPermission(Manifest.permission.SEND_SMS)) {
+            mSendMessageBtn.setEnabled(true);
+        }else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS},
+                    SEND_SMS_PERMISSION_REQUEST_CODE);
+        }
+        mSendMessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String message = messagetEt.getText().toString();
+                //String phoneNo = mPhoneNoEt.getText().toString();
+               // if(!TextUtils.isEmpty(message) && !TextUtils.isEmpty(phoneNo)) {
+
+                   // if(checkPermission(Manifest.permission.SEND_SMS)) {
+                       // SmsManager smsManager = SmsManager.getDefault();
+                      //  smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                    //}else {
+                    //    Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                  //  }
+                //}
+                send();
             }
         });
 
@@ -40,6 +96,22 @@ public class MainActivity extends AppCompatActivity {
         // ...
         mTelephonyManager = (TelephonyManager) getSystemService(getApplicationContext().TELEPHONY_SERVICE);
 
+    }
+    private boolean checkPermission(String permission) {
+        int checkPermission = ContextCompat.checkSelfPermission(this, permission);
+        return (checkPermission == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case SEND_SMS_PERMISSION_REQUEST_CODE: {
+                if(grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    mSendMessageBtn.setEnabled(true);
+                }
+                return;
+            }
+        }
     }
     PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
         @Override
@@ -68,5 +140,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+    }
+    public void send(){
+        txtNumero = number.getText().toString();
+        txtMensaje = message.getText().toString();
+
+        String phoneNumber = this.txtNumero;
+        String smsMessage = this.txtMensaje;
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            if(phoneNumber.equals(new PhoneCallStateReceiver().outgoingPhoneNo)){
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNumber,null,smsMessage,null,null);
+                Toast.makeText(this,"Mensaje enviado..",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Mensaje No enviado..",Toast.LENGTH_LONG).show();
+
+            }
+        }else{
+            Toast.makeText(this,"Permiso negado",Toast.LENGTH_LONG).show();
+
+        }
     }
 }
